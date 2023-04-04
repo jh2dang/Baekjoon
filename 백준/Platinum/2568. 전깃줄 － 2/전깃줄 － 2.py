07c4cec48c49cc6c94b416849ef0d175eff8745b
jh2dang <122415714+jh2dang@users.noreply.github.com>
2023-04-04 19:51:16 +0900

    [Platinum V] Title: 전깃줄 - 2, Time: 316 ms, Memory: 50536 KB -BaekjoonHub

--- 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.py
+++ 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.py
@@ -3,6 +3,7 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
+
 zul = []
 
 for n in range(N):
@@ -11,13 +12,19 @@ for n in range(N):
 
 zul.sort()
 
+# zul = [[1, 8], [2, 2], [3, 9], [4, 1], [6, 4], [7, 6], [9, 7], [10, 10]]
+
+
 A = [0]*N
 
 for n in range(N):
     A[n] = zul[n][1]
 
+# A = [8, 2, 9, 1, 4, 6, 7, 10]
+
+# ====================여기부터 LIS 만들기 시작
 L = []
-dp = [0]*N
+dp = [0]*N      # 인덱스 저장하는 리스트
 
 for i in range(N):
     if not L or L[-1] < A[i]:
@@ -27,6 +34,10 @@ for i in range(N):
         idx = bisect_left(L, A[i])
         L[idx] = A[i]
         dp[i] = idx
+
+# L = [1, 4, 6, 7, 10]
+# dp = [0, 0, 1, 0, 1, 2, 3, 4]
+
 
 MAX = max(dp)
 rlt = [0]*(MAX+1)
@@ -40,6 +51,10 @@ for j in range(len(dp)-1, -1, -1):
         DEL[cnt] = zul[j][0]
         cnt += 1
 DEL.sort()
+
+
+# rlt = [1, 4, 6, 7, 10]
+# DEL = [1, 2, 3] (dp에서 rlt뺀것)
 
 
 print(len(DEL))
