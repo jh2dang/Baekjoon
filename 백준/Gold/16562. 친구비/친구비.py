aa77dd836d7ba59292984dc83b26ec9cc1cf1f3f
jh2dang <122415714+jh2dang@users.noreply.github.com>
2023-04-06 14:13:57 +0900

    [Gold IV] Title: 친구비, Time: 1624 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.py
+++ 백준/Gold/16562. 친구비/친구비.py
@@ -21,29 +21,24 @@ for _ in range(M):
             for i in range(N+1):
                 if V[i] == MAX:
                     V[i] = MIN
-# print(V)
+
 v = [0]*(num+1)
-G = [[0] for _ in range(N+1)]
-for i in range(1, N+1):
-    if V[i] != 0:
-        if not v[V[i]]:
-            G[V[i]] = [1, don[i-1]]
-            v[V[i]] = 1
-        else:
-            G[V[i]][0] += 1
-            if don[i-1] < G[V[i]][1]:
-                G[V[i]][1] = don[i-1]
-# print(G)
-# print(V)
+G = [1e9]*(num+1)
 SUM = 0
-for g in range(len(G)):
-    if len(G[g]) > 1:
-        SUM += G[g][1]
 
 for i in range(1, N+1):
-    if not V[i]:
+    if V[i] == 0:
         SUM += don[i-1]
+        if SUM > K:
+            break
+    else:
+        if don[i-1] < G[V[i]]:
+            G[V[i]] = don[i-1]
+for g in range(len(G)):
+    if G[g] != 1e9:
+        SUM += G[g]
+        if SUM > K:
+            break
 if SUM > K:
     print('Oh no')
-else:
-    print(SUM)
+else: print(SUM)
